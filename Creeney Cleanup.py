d46e189634f904d26eae05bcef65e53c307435b2
Tony Creeney <tonycreeney@gmail.com>
2024-05-13 23:10:09 +0100

    added parts of the delete temp files method--not finished (doesnt work)

--- Creeney Cleanup.py
+++ Creeney Cleanup.py
@@ -1,24 +1,32 @@
 import shutil
 
+#deletes tempory file
 def delete_temp_files():
-    file = 'file:///C:/WINDOWS/Temp/'
+    file = "C:\\WINDOWS\\Temp\\"
 
-    shutil.
-def delete_cache_file():
+    #trys to remove the error, if it fails, it displays the error for you to read
+    try:    
+        shutil.rmtree(file)
+        print(f"Contents of {file} has been deleted SUCCESSFULLY!")
+    except OSError as e:
+        print(f"Error! Directory: {file} : {e.strerror}")
+
+#def delete_cache_file():
     
-def delete_old_restore_points():
+#def delete_old_restore_points():
 
-def delete_windows_update():
+#def delete_windows_update():
 
-def delete_log_reports():
+#def delete_log_reports():
 
-def delete_unused_apps():
+#def delete_unused_apps():
 
-def delete_large_files():
+#def delete_large_files():
 
-def empty_recycle_bin():
+#def empty_recycle_bin():
 
-def delete_old_downloads():
+#def delete_old_downloads():
 
-def delete_browser_cookies():
-    
+#def delete_browser_cookies():
+
+delete_temp_files()
